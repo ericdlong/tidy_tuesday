01c6a61a1f374bb3587edc9a373b5193052f8fcf
ericdlong <long.eric.d@gmail.com>
2022-02-23 13:28:41 -0600

    Commit from RStudio

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tidy_tuesday
 Repo for Tidy Tuesday Exercises <br />
+<br />
+Citation: <br />
+Thomas Mock (2022). Tidy Tuesday: A weekly data project aimed at the R ecosystem.<br />
 https://github.com/rfordatascience/tidytuesday
--- tt_freedom_2_22_2022.R
+++ tt_freedom_2_22_2022.R
@@ -1,2 +1,20 @@
 #Tidy Tuesday - 2/22/2022
 #Eric Long
+
+#####Load Libraries
+
+library(tidytuesdayR)
+library(tidyverse)
+
+#####Read in data, code taken from https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-02-22
+
+# Get the Data
+
+# Read in with tidytuesdayR package 
+# Install from CRAN via: install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works!
+
+tuesdata <- tidytuesdayR::tt_load('2022-02-22')
+freedom <- tuesdata$freedom
